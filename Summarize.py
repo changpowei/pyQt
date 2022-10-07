03067fa7f8ed10c22fd6ff6e12278aab9967d50d
c95cpw <c95cpw@ncsist.org.tw>
2022-10-07 16:30:37 +0800

    feat:修正爬蟲與摘要程式，設計UI界面

--- Summarize.py
+++ Summarize.py
@@ -6,107 +6,120 @@ import requests
 import json
 import base64
 from opencc import OpenCC
-import urllib, sys
+import urllib
 import ssl
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+
+class Summarizer(QThread):
+    sinOut = pyqtSignal(str)  # 自定義訊號，執行run()函式時，從相關執行緒發射此訊號
+    def __init__(self, data, load_path, save_path):
+        super(Summarizer, self).__init__()
+        self.data = data
+        self.load_path = load_path + self.data + '_本文.docx'
+        self.save_path = save_path + self.data + '_摘要.docx'
+    def TW2S(self, article):
+        # 繁體轉簡體
+        cc = OpenCC('tw2s')
+        return cc.convert(article)
+
+    def S2TW(self, article):
+        # 簡體轉繁體
+        cc = OpenCC('s2tw')
+        return cc.convert(article)
 
 
-def TW2S(article):
-    # 繁體轉簡體
-    cc = OpenCC('tw2s')
-    return cc.convert(article)
+    def Summarizing_free(self, one_article):
+        try:
+            token = 'grv8ObC13pwUfffZ'
+            taskID = 'zhai0904'
 
-def S2TW(article):
-    # 簡體轉繁體
-    cc = OpenCC('s2tw')
-    return cc.convert(article)
+            payload = {'token': token, 'taskid': taskID, 'con':one_article}
+            Summarized = requests.post("http://inter.xiaohuaerai.com/api/task", data=payload)
+            # print(json.loads(Summarized.text)["data"]["contents"])
 
+            return json.loads(Summarized.text)["data"]["contents"]
 
-def Summarizing_free(one_article):
-    try:
-        token = 'grv8ObC13pwUfffZ'
-        taskID = 'zhai0904'
+        except:
 
-        payload = {'token': token, 'taskid': taskID, 'con':one_article}
-        Summarized = requests.post("http://inter.xiaohuaerai.com/api/task", data=payload)
-        # print(json.loads(Summarized.text)["data"]["contents"])
+            return "文章無法抽取摘要，請自行剪貼!"
 
-        return json.loads(Summarized.text)["data"]["contents"]
+    def Summarizing_paid(self, one_article):
+        try:
+            host = 'https://zhaiyao.xiaohuaerai.com'
+            path = '/zhaiyao'
+            method = 'POST'
+            appcode = '449f24b2833844c6a5b0b51fed1b6f3e'
+            querys = ''
+            bodys = {}
+            url = host + path
 
-    except:
+            bodys['src'] = one_article
+            post_data = urllib.parse.urlencode(bodys).encode("utf-8")
+            request = urllib.request.Request(url, post_data)
+            request.add_header('Authorization', 'APPCODE ' + appcode)
+            # 根据API的要求，定义相对应的Content - Type
+            request.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+            response = urllib.request.urlopen(request, context=ctx)
+            content = response.read()
+            # if (content):
+            #     print(json.loads(content)["res"])
+            return json.loads(content)["res"]
 
-        return "文章無法抽取摘要，請自行剪貼!"
+        except Exception as e :
 
-def Summarizing_paid(one_article):
-    try:
-        host = 'https://zhaiyao.xiaohuaerai.com'
-        path = '/zhaiyao'
-        method = 'POST'
-        appcode = '449f24b2833844c6a5b0b51fed1b6f3e'
-        querys = ''
-        bodys = {}
-        url = host + path
+            print(e)
+            return "文章無法抽取摘要，請自行剪貼!"
 
-        bodys['src'] = one_article
-        post_data = urllib.parse.urlencode(bodys).encode("utf-8")
-        request = urllib.request.Request(url, post_data)
-        request.add_header('Authorization', 'APPCODE ' + appcode)
-        # 根据API的要求，定义相对应的Content - Type
-        request.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        response = urllib.request.urlopen(request, context=ctx)
-        content = response.read()
-        # if (content):
-        #     print(json.loads(content)["res"])
-        return json.loads(content)["res"]
+    def GetSummarize(self, load_path, save_path):
 
-    except Exception as e :
+        load_content = Document(load_path)
+        summarized_content = Document()
 
-        print(e)
-        return "文章無法抽取摘要，請自行剪貼!"
+        one_article = ""
+        question_nums = 0
+        for i, paragraph in enumerate(load_content.paragraphs):
 
-def GetSummarize(load_path, save_path):
+            if paragraph.style.name == 'Heading 1':
+                question_nums += 1
+                summarized_content.add_heading(paragraph.text, level=1)
+                self.sinOut.emit("第{}題: {}".format(question_nums, paragraph.text))
+                print("第{}題: {}".format(question_nums, paragraph.text))
 
-    load_content = Document(load_path)
-    summarized_content = Document()
+                one_article = ""
 
-    one_article = ""
-    question_nums = 0
-    for i, paragraph in enumerate(load_content.paragraphs):
-
-        if paragraph.style.name == 'Heading 1':
-            question_nums += 1
-            summarized_content.add_heading(paragraph.text, level=1)
-            print("第{}題: {}".format(question_nums, paragraph.text))
-            one_article = ""
-
-        else:
-            if paragraph.text != "" and paragraph.text != "\n":
-                    one_article += paragraph.text
+            else:
+                if paragraph.text != "" and paragraph.text != "\n":
+                        one_article += paragraph.text
 
 
-            if i == len(load_content.paragraphs)-1:
-                if one_article != []:
-                    Transfering(summarized_content, one_article)
+                if i == len(load_content.paragraphs)-1:
+                    if one_article != []:
+                        self.Transfering(summarized_content, one_article)
 
-            elif  load_content.paragraphs[i+1].style.name == 'Heading 1':
-                Transfering(summarized_content, one_article)
+                elif  load_content.paragraphs[i+1].style.name == 'Heading 1':
+                    self.Transfering(summarized_content, one_article)
 
-    summarized_content.save(save_path)
+        summarized_content.save(save_path)
 
-def Transfering(summarized_content, one_article):
+    def Transfering(self, summarized_content, one_article):
 
-    summarized_content.add_paragraph(Summarizing_paid(one_article))
-    summarized_content.add_paragraph()
-    summarized_content.add_page_break()
+        summarized_content.add_paragraph(self.Summarizing_free(one_article))
+        summarized_content.add_paragraph()
+        summarized_content.add_page_break()
 
 
-if __name__ == '__main__':
+    def run(self):
 
-    data = 'test'
+        # data = 'test'
 
-    load_path = './本文/' + data + '_本文.docx'
-    save_path = './摘要/' + data + '_摘要.docx'
+        # load_path = './本文/' + self.data + '_本文.docx'
+        # save_path = './摘要/' + self.data + '_摘要.docx'
 
-    GetSummarize(load_path, save_path)
+        self.GetSummarize(self.load_path, self.save_path)
+
+        self.sinOut.emit("\n文章摘要完成")
--- pyQt5.py
+++ pyQt5.py
@@ -6,6 +6,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 import datetime
+from News_scrawler import *
+from Summarize import *
+from os.path import exists
+
 
 class MyWidget(QWidget):
     def __init__(self):
@@ -13,7 +17,9 @@ class MyWidget(QWidget):
         self.content_save_location = './本文/'
         self.abstract_save_location = './摘要/'
         self.file_save_name = str(datetime.date.today())
+        self.all_txt = ''
         self.initUI()
+
 
     def initUI(self):
         self.setWindowTitle('馬董早')
@@ -63,44 +69,75 @@ class MyWidget(QWidget):
         self.abstract_output_btn = QPushButton('摘要儲存位置', self)
         self.abstract_output_btn.clicked.connect(self.AbstractButtonClick)
 
-        self.scrawler_btn = QPushButton('擷取全文', self)
-        self.scrawler_btn.clicked.connect(self.scratchButtonClick)
+        self.crawler_btn = QPushButton('擷取全文', self)
+        self.crawler_btn.clicked.connect(self.scratchButtonClick)
+        self.crawler_btn.setEnabled(True)
 
         self.abstract_btn = QPushButton('擷取摘要', self)
         self.abstract_btn.clicked.connect(self.abstractButtonClick)
+        self.abstract_btn.setEnabled(True)
 
         self.reset_btn = QPushButton('返回預設值', self)
         self.reset_btn.clicked.connect(self.resetButtonClick)
 
-        self.show_infoes_signal = QLabel(self)
-        self.show_infoes_signal.setStyleSheet("background-color: Gainsboro")
+        self.show_infoes = QLabel(self)
+        self.show_infoes.setStyleSheet("background-color: Gainsboro")
+        self.show_infoes.setAlignment(Qt.AlignTop)
+        self.show_infoes.setScaledContents(True)
+
+        self.scroll_area = QScrollArea(self)
+        self.scroll_area.setWidgetResizable(True)
 
         self.AllLayout(layout)
 
     def ContentButtonClick(self):
         folderPath = QtWidgets.QFileDialog.getExistingDirectory()  # 選取特定資料夾
-        self.content_save_location = folderPath
+        self.content_save_location = folderPath + '/'
         folder_split = folderPath.split('/')
-        new_folder = folder_split[-2] + '/' + folder_split[-1]
+        new_folder = '.../' + folder_split[-2] + '/' + folder_split[-1] + '/'
         self.content_folder_label.setText(new_folder)
-        print(folderPath)
+        print(self.content_save_location)
 
     def AbstractButtonClick(self):
         folderPath = QtWidgets.QFileDialog.getExistingDirectory()  # 選取特定資料夾
-        self.abstract_save_location = folderPath
+        self.abstract_save_location = folderPath + '/'
         folder_split = folderPath.split('/')
-        new_folder = folder_split[-2] + '/' + folder_split[-1]
+        new_folder = '.../' + folder_split[-2] + '/' + folder_split[-1] + '/'
         self.abstract_folder_label.setText(new_folder)
-        print(folderPath)
+        print(self.abstract_save_location)
 
     def scratchButtonClick(self):
-        self.scrawler_btn.setText('文章擷取中...')
-        self.scrawler_btn.setStyleSheet("background-color: silver")
+        if self.crawler_btn.isEnabled and self.abstract_btn.isEnabled:
+            if self.text_input.toPlainText() != "":
+                self.crawler_btn.setDisabled(True)
+                self.file_save_name = self.file_name.text()
+                self.all_txt = self.text_input.toPlainText()
+
+                self.news_crawler = News_scrawler(all_txt = self.all_txt, data=self.file_save_name, docx_save_loc=self.content_save_location)
+                self.news_crawler.sinOut.connect(self.show_info)
+                self.news_crawler.start()
+            else:
+                self.show_info("請於左方欄位輸入欲進行文章抓取的內容！")
 
     def abstractButtonClick(self):
-        self.abstract_btn.setText('摘要擷取中...')
-        self.abstract_btn.setStyleSheet("background-color: silver")
+        if self.abstract_btn.isEnabled and self.crawler_btn.isEnabled:
+            if exists(self.content_save_location + self.file_save_name + '_本文.docx'):
+                self.abstract_btn.setDisabled(True)
 
+                self.summarize = Summarizer(data = self.file_save_name, load_path = self.content_save_location, save_path = self.abstract_save_location)
+                self.summarize.sinOut.connect(self.show_info)
+                self.summarize.start()
+            else:
+                self.show_info("本文不存在，請先執行爬蟲！")
+
+    def show_info(self, infos):
+        previous_text = self.show_infoes.text() + "\n"
+        self.show_infoes.setText(previous_text + infos)
+        if infos == "\n文章爬蟲完成":
+            self.crawler_btn.setEnabled(True)
+
+        if infos == "\n文章摘要完成":
+            self.abstract_btn.setEnabled(True)
     def resetButtonClick(self):
         self.content_save_location = './本文/'
         self.content_folder_label.setText(self.content_save_location)
@@ -108,6 +145,9 @@ class MyWidget(QWidget):
         self.abstract_folder_label.setText(self.abstract_save_location)
         self.file_save_name = str(datetime.date.today())
         self.file_name.setText(self.file_save_name)
+        self.text_input.setText("")
+        self.show_infoes.setText("")
+        self.all_txt = self.text_input.toPlainText()
 
 
     def openFolder(self):
@@ -125,13 +165,14 @@ class MyWidget(QWidget):
         layout.addWidget(self.text_input, 2, 0, 10, 8)
         layout.addWidget(self.output_file, 2, 8, 1, 2)
         layout.addWidget(self.file_name, 2, 10, 1, 2)
-        layout.addWidget(self.scrawler_btn, 10, 8, 1, 4)
+        layout.addWidget(self.crawler_btn, 10, 8, 1, 4)
         layout.addWidget(self.abstract_btn, 11, 8, 1, 4)
-        layout.addWidget(self.show_infoes_signal, 3, 8, 7, 4)
+        layout.addWidget(self.show_infoes, 3, 8, 7, 4)
         layout.addWidget(self.reset_btn, 0, 0, 1, 2)
+        layout.addWidget(self.scroll_area, 3, 8, 7, 4)
 
-        # layout.addWidget()
-        # layout.addWidget(self.mybutton, 50, 50, 1, 1)
+        self.scroll_area.setWidget(self.show_infoes)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
